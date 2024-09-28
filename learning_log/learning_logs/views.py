13e8a08d66d311949c187fc03ad29d3cc9527e0a
notebook_pessoal <professorjailsoninformatica@gmail.com>
2024-09-28 08:48:27 -0300

    Aula 8 - Entendendo melhor o "block e utilizando um "for" dentro da página".

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     # """ Página principal do Learning_Log """"
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    # """ mostra todos os assuntos """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
